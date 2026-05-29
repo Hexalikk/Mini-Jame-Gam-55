eaf42c117c42d5c3568370dd3434cfd8822c28bd
Nina Akhumyan <nina.akhumyan@etu.unistra.fr>
2026-05-29 18:46:55 +0200

    animation jump et throw

--- stronger-bones/scripts/player.gd
+++ stronger-bones/scripts/player.gd
@@ -12,6 +12,7 @@ var isHeldEnough: bool = false
 
 
 signal drop_a_bone
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 enum STATE {
@@ -22,27 +23,39 @@ enum STATE {
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if is_on_floor() and velocity.x == 0 and not Input.is_action_pressed("jump"):
+		if (_state == STATE.NORMAL):
+			animated_sprite.play("default")
+		if (_state == STATE.NO_LEGS):
+			animated_sprite.play("throw_legs")
+		if (_state == STATE.HEAD):
+			animated_sprite.play("throw_body")
+		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	if Input.is_action_pressed("jump") and is_on_floor():
-		heldFrameCounter += 1 
+		animated_sprite.play("jump_left")
+		heldFrameCounter += 1
 		if heldFrameCounter > 50:
 			heldFrameCounter = 51
 			isHeldEnough = true
 		
 	if Input.is_action_just_released("jump") and is_on_floor():
+		animated_sprite.play("jump_left")
 		if isHeldEnough == true && _state == STATE.NORMAL :
 			velocity.y += JUMP_VELOCITY*2
 			isHeldEnough = false
 			heldFrameCounter = 0
 			print("long jump")
+
 		elif _state != STATE.HEAD:
 			isHeldEnough = false
 			heldFrameCounter = 0
 			velocity.y += JUMP_VELOCITY
 		else:
 			velocity.y += SKULL_JUMP_VELOCITY
+
 	
 	if Input.is_action_just_released("throw a bone"):
 		match(_state):
