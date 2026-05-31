a5c0a100bf41e52ff20fac830c2a3ca77e2f8a64
Clément kaiser <clement.kaiser@etu.unistra.fr>
2026-05-31 03:44:28 +0200

    game manager

--- stronger-bones/scripts/game_manager.gd
+++ stronger-bones/scripts/game_manager.gd
@@ -4,6 +4,7 @@ var _current_level = -1
 
 var level_1 =preload("res://scenes/levels/Level1.tscn")
 var level_2 = preload("res://scenes/levels/Level2.tscn")
+@onready var pause = $PauseMenu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,7 +13,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("pause"):
+		print("test")
+		pause.show()
 
 func load_level(id :int):
 	var level
@@ -33,3 +36,11 @@ func _on_ui_controler_load_level() -> void:
 
 func _switch_level():
 	load_level(_current_level+1)
+
+
+func _on_pause_menu_return_to_menu() -> void:
+	var childs = get_node(".").get_children()
+	for c in childs:
+		c.free()
+	$"../UiControler".show()
+	
